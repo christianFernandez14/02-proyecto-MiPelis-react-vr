13bcb730975a9281bb476b71790eaf2cb4346030
Christian Fernandez <christian14@gmail.com>
2024-02-09 13:23:22 -0300

    Adaptando la maquetacion al proyecto de React / mano al Css tambien / .74

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 -- Poreycto sobre Peliculas con React --
+
+
+1.- En limpieza del entorno de trabajos, nos encontramos con otra manera de cargar el css como un archivo externo y no sobre escribiendo el index.css que viene por defecto.
+
+  Caso curioso: apretar crtl + u; y te dara un detalle del html de la pagina
+
+2.- Vamos a trabajar con desde una maquetación creada en HTML + CSS, la idea es que traigas todo el codigo HTML y ahi iremos desglozando por componente lo que sea necesario
--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,82 @@
 const App = () => {
 
   return (
-    <div>
-      <h1>from App Component</h1>
+    <div className="layout">
+
+      {/* Cebecera  */}
+      <header className="header">
+       <div className="logo">
+        <div className="play"></div>
+       </div>
+        <h1>MisPelis</h1>
+      </header>
+
+      {/* Barra de navegacion */}
+      <nav className="nav">
+        <ul>
+          <li><a href="#">Inicio</a></li>
+          <li><a href="#">Pelicula</a></li>
+          <li><a href="#">Blog</a></li>
+          <li><a href="#">Contacto</a></li>
+        </ul>
+      </nav>
+
+     {/* Contenido principal */}
+      <section className="content">
+        
+        <article className="peli-item">
+          <h3 className="title">Desarrollo web</h3>
+          <p className="description">christianweb.cl</p>
+          <button className="edit">Editar</button>
+          <button className="delete">Borrar</button>
+        </article>
+
+        <article className="peli-item">
+          <h3 className="title">Desarrollo web</h3>
+          <p className="description">christianweb.cl</p>
+          <button className="edit">Editar</button>
+          <button className="delete">Borrar</button>
+        </article>
+
+        <article className="peli-item">
+          <h3 className="title">Desarrollo web</h3>
+          <p className="description">christianweb.cl</p>
+          <button className="edit">Editar</button>
+          <button className="delete">Borrar</button>
+        </article>
+
+        <article className="peli-item">
+          <h3 className="title">Desarrollo web</h3>
+          <p className="description">christianweb.cl</p>
+          <button className="edit">Editar</button>
+          <button className="delete">Borrar</button>
+        </article>
+
+      </section>
+
+     {/* Barra lateral */}
+      <aside className="lateral">
+        
+        <div className="search">
+          <h3 className="title">Buscador</h3>
+          <form>
+            <input type="text" />
+            <button>Buscar</button>
+          </form>
+        </div>
+
+        <div className="add">
+          <h3 className="title">Añadir pelicula</h3>
+          <form>
+            <input type="text" placeholder="Titulo" />
+            <textarea placeholder="Descripcion"></textarea>
+            <input type="submit" value="Guardar" />
+          </form>
+        </div>
+      </aside>
+
+      {/* Pie de pagina */}
+      <footer className="footer">&copy; Master en JavaScript E12 y TypeScript</footer>
     </div>
   )
 }
