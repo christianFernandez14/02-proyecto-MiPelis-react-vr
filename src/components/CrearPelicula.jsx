50b7636d8a7251e3659ff932f15eb684229f9ef4
Christian Fernandez <christian14@gmail.com>
2024-02-09 14:08:05 -0300

    Creando el formulario para crear cada Pelicula / .76

--- src/components/CrearPelicula.jsx
+++ src/components/CrearPelicula.jsx
@@ -1,12 +1,65 @@
+import { useState } from "react"
 
 const CrearPelicula = () => {
+
+  const [peliSate, setPeliSate] = useState({
+    titulo: '',
+    descripcion: ''
+  })
+
+  const {titulo, descripcion} = peliSate
+
+
+  const titleComponent = 'Añadir pelicula'
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    // Tomamos los datos del fomulario
+
+    const target = e.target
+    const titulo = target.titulo.value
+    const descripcion = target.descripcion.value
+
+    // Creamos el objeto de cada pelicula a guardar
+    const pelicula = {
+      id: new Date().getTime(),
+      titulo,
+      descripcion
+    }
+
+    setPeliSate(pelicula)
+
+  }
+
+
   return (
     <div className="add">
-      <h3 className="title">Añadir pelicula</h3>
-      <form>
-        <input type="text" placeholder="Titulo" />
-        <textarea placeholder="Descripcion"></textarea>
-        <input type="submit" value="Guardar" />
+      <h3 className="title">{titleComponent}</h3>
+
+      <strong>
+        {(titulo && descripcion) && 'Haz Añadido: '+titulo}
+      </strong>
+
+      <form onSubmit={handleSubmit}>
+
+        <input
+          type="text"
+          placeholder="Titulo"
+          name="titulo"
+          id="titulo"
+        />
+        <textarea
+          id="descripcion"
+          name="descripcion"
+          placeholder="Descripcion"
+        ></textarea>
+        <input
+          type="submit"
+          id="save"
+          value="Guardar"
+        />
+
       </form>
     </div>
   )
