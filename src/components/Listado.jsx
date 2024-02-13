0838482a35792e74a39044a19757957adf87d288
Christian Fernandez <christian14@gmail.com>
2024-02-13 08:53:16 -0300

    Listando las peliculas que se encuentra en el LocalStorage / mapeando / .79

--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -1,34 +1,37 @@
+import { useEffect, useState } from "react"
 
 const Listado = () => {
+
+  const [listadoState, setListadoState] = useState([])
+
+  useEffect(() => {
+    conseguirPeliculas()
+  }, [])
+
+
+  const conseguirPeliculas = () => {
+    let peliculas = JSON.parse(localStorage.getItem('Pelicula'))
+
+    setListadoState(peliculas)
+  }
+
+
   return (
     <>
-      <article className="peli-item">
-        <h3 className="title">Desarrollo web</h3>
-        <p className="description">christianweb.cl</p>
-        <button className="edit">Editar</button>
-        <button className="delete">Borrar</button>
-      </article>
+      {!listadoState 
+      ? <h1>No hay peliculas que mostrar</h1>
+      : listadoState.map(peli => (
+        <article
+          key={peli.id}
+          className="peli-item"
+        >
+          <h3 className="title">{peli.titulo}</h3>
+          <p className="description">{peli.descripcion}</p>
+          <button className="edit">Editar</button>
+          <button className="delete">Borrar</button>
+        </article>
 
-      <article className="peli-item">
-        <h3 className="title">Desarrollo web</h3>
-        <p className="description">christianweb.cl</p>
-        <button className="edit">Editar</button>
-        <button className="delete">Borrar</button>
-      </article>
-
-      <article className="peli-item">
-        <h3 className="title">Desarrollo web</h3>
-        <p className="description">christianweb.cl</p>
-        <button className="edit">Editar</button>
-        <button className="delete">Borrar</button>
-      </article>
-
-      <article className="peli-item">
-        <h3 className="title">Desarrollo web</h3>
-        <p className="description">christianweb.cl</p>
-        <button className="edit">Editar</button>
-        <button className="delete">Borrar</button>
-      </article>
+      ))}
     </>
   )
 }
